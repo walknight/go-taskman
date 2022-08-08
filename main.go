04ba93fc582748a0f88d56b7fc886b9ec069174d
walknight <darkwalkernight@gmail.com>
2022-08-08 21:42:02 +0700

    add http server and html template

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var data = map[string]string{
+			"Name":    "John Wick",
+			"Message": "Hello world!",
+		}
+
+		var t, err = template.ParseFiles("template/master.html")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		t.Execute(w, data)
+	})
+
+	fmt.Println("Starting web server at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
